8d8c391f4536f63e89acb82bc2988c80554000c2
Max Beneke <max.beneke@gmail.com>
2021-01-08 15:28:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,7 @@ def game_hash
 end
 
 def num_points_scored(player_name)
-  game_hash.each do |location|
+  game_hash.each do |location, |
     location.each do |attribute, value|
       binding.pry
 end
